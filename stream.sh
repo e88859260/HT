d5d78c0f1391347a7f5686ffaec3e6696e88369a
HT <52118783+e88859260@users.noreply.github.com>
2020-05-12 21:46:03 +0800

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -3,7 +3,7 @@ sudo apt install -y wget
 sudo apt install -y python-pip
 pip install streamlink
 apt install -y screen
-wget https://iwall.ipins.workers.dev/ffmepg.tar.gz
+wget https://iwall.ipins.workers.dev/ffmpeg.tar.gz
 tar xvf ffmpeg.tar.gz
 rm -f ffmpeg.tar.gz
 sudo mv ffmpeg ffprobe /usr/bin/
